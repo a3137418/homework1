ab51b3552172d35616cea48dd0bcc892c21a7d28
a3137418 <a3137418@gmail.com>
2025-12-09 09:50:48 +0800

    A finish

--- homework1/myapp/views.py
+++ homework1/myapp/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def test(request):
     return HttpResponse("test")
 def aweb(request):
-    return HttpResponse("hello,world")
+    return HttpResponse("hello,world,finish")

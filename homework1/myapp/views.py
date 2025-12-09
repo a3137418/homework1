b0f9961058abf831a5463054f6e2ac0d015dfeaa
a3137418 <a3137418@gmail.com>
2025-12-09 09:30:33 +0800

    A update1

--- homework1/myapp/views.py
+++ homework1/myapp/views.py
@@ -1,5 +1,8 @@
+#userA
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request):
+    return HttpResponse("test")
+def aweb(request):
     return HttpResponse("test")
